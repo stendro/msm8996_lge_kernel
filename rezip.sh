fb31b67f605ea45d78d56719277bf2d374798b91
stendro <33956801+stendro@users.noreply.github.com>
2018-03-15 16:41:59 +0100

    build: add version support in ak
    Grabbed this from ak repository.
    Also add a script to create archives from existing folders
    in out directory.

--- rezip.sh
+++ rezip.sh
@@ -2,20 +2,19 @@
 #
 # Created by stendro. Based on build script by jcadduono.
 #
-# This script copies the dtb-embedded kernel, and modules, to the *out* directory.
-# It also copies mk2000 specific AnyKernel2 files and creates a flashable zip.
-# You can simply run ./copy_finished.sh after doing ./build.sh - it knows which device you built for.
+# This script builds zips from the existing folders in the "out" directory.
+# This is so that you can make adjustments to the zip without recompiling
+# the entire kernel. Specify the device, i.e ./build_zip.sh H850
 
 RDIR=$(pwd)
-BDIR=${RDIR}/build
 
 ABORT() {
 	echo "Error: $*"
 	exit 1
 }
 
-DEVICE=$(cat "${BDIR}/DEVICE") \
-		|| ABORT "No device file found in ${BDIR}"
+[ "$1" ] && DEVICE=$1
+[ "$DEVICE" ] || ABORT "No device specified"
 
 VER=$(cat "${RDIR}/VERSION") \
 		|| ABORT "No version file found in ${RDIR}"
@@ -24,27 +23,15 @@ BVER=$(cat ${RDIR}/VERSION | cut -f1 -d'-')
 
 RDISK=${RDIR}/ramdisk
 AK_DIR=${RDISK}/ak-script
-MOD_DIR=${BDIR}/lib/modules
-KERN_DIR=${BDIR}/arch/arm64/boot
 BANNER_BETA=${RDISK}/banner-beta
 INIT_FILE_G6=${RDISK}/init-g6
 DDIR=${RDIR}/out/${DEVICE}
 INIT_FILE=${RDISK}/init
 BANNER=${RDISK}/banner
 
-CLEAN_DIR() {
-	echo "Cleaning folder..."
-	rm -rf $DDIR
+CLEAN_ZIP() {
+	echo "Remove old zip..."
 	rm -f $RDIR/out/${DEVICE}_${VER}-mk2000.zip
-}
-
-SETUP_DIR() {
-	echo "Setting up folder..."
-	mkdir -p $RDIR/out
-	unzip -q $RDISK/ak-root.zip -d $DDIR \
-		|| ABORT "Failed to unzip *ak-root.zip*"
-	cp $RDISK/update-binary $DDIR/META-INF/com/google/android \
-		|| ABORT "Failed to copy *update-binary*"
 }
 
 COPY_AK() {
@@ -74,22 +61,6 @@ COPY_INIT() {
 	fi
 }
 
-COPY_KERNEL() {
-	if [ "$DEVICE" = "H850" ] || [ "$DEVICE" = "H830" ] || [ "$DEVICE" = "RS988" ]; then
-	  echo "Copying kernel and modules (G5)..."
-	  cp $KERN_DIR/Image.lz4 $DDIR \
-		|| ABORT "Failed to copy kernel"
-	  python $RDISK/dtbTool -o $DDIR/dt.img -s 4096 $KERN_DIR/dts \
-		|| ABORT "Failed to create dt.img - do you have libfdt installed?"
-	else
-	  echo "Copying kernel and modules..."
-	  cp $KERN_DIR/Image.lz4-dtb $DDIR \
-		|| ABORT "Failed to copy kernel"
-	fi
-	find $MOD_DIR/ -name '*.ko' -exec cp {} $DDIR/modules \; \
-		|| ABORT "Failed to copy modules"
-}
-
 ZIP_UP() {
 	echo "Creating AnyKernel2 archive..."
 	cd $DDIR
@@ -100,11 +71,9 @@ ZIP_UP() {
 cd "$RDIR" || ABORT "Failed to enter ${RDIR}"
 echo "Preparing ${DEVICE} ${VER}"
 
-CLEAN_DIR &&
-SETUP_DIR &&
+CLEAN_ZIP &&
 COPY_AK &&
 COPY_INIT &&
-COPY_KERNEL &&
 ZIP_UP &&
 echo "Finished!"
 echo "Look in *out* folder"
