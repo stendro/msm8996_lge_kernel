cc0decc56f097021a603b608989391649e0b3523
Rashed Abdel-Tawab <rashed@linux.com>
2018-03-17 17:40:30 -0700

    Revert "usb: f_fs: Prevent gadget unbind if it is already unbound"
    This reverts commit 43cf1b8adbfbad80ae6eec2f7de8fd7c104df823.

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -3610,8 +3610,7 @@ static void ffs_closed(struct ffs_data *ffs)
 	ci = opts->func_inst.group.cg_item.ci_parent->ci_parent;
 	ffs_dev_unlock();
 
-	if (test_bit(FFS_FL_BOUND, &ffs->flags))
-		unregister_gadget_item(ci);
+	unregister_gadget_item(ci);
 	return;
 done:
 	ffs_dev_unlock();
