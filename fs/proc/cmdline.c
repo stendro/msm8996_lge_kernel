70abc6c6a93d2c8d3e85ae63fa38eeabb25f726d
NateM <nathan90@gmail.com>
2018-03-25 11:10:59 -0400

    proc: modify cmdline to export correct charger mode
    * Force chargerlogo mode for charger
    fixes offmode charging
    
    Change-Id: Ibf6295cc4ddce432690fd557e13d630a752cd73b

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -2,10 +2,43 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <soc/qcom/lge/board_lge.h>
+#include <asm/setup.h>
+#include <linux/slab.h>
+
+static char updated_command_line[COMMAND_LINE_SIZE];
+
+static void proc_cmdline_set(char *name, char *value)
+{
+	char *flag_pos, *flag_after;
+	char *flag_pos_str = kmalloc(sizeof(char), COMMAND_LINE_SIZE);
+
+	scnprintf(flag_pos_str, COMMAND_LINE_SIZE, "%s=", name);
+
+	flag_pos = strstr(updated_command_line, flag_pos_str);
+	if (flag_pos) {
+		flag_after = strchr(flag_pos, ' ');
+		if (!flag_after)
+			flag_after = "";
+
+		scnprintf(updated_command_line, COMMAND_LINE_SIZE, "%.*s%s=%s%s",
+				(int)(flag_pos - updated_command_line),
+				updated_command_line, name, value, flag_after);
+	} else {
+		// flag was found, insert it
+		scnprintf(updated_command_line, COMMAND_LINE_SIZE, "%s %s=%s", updated_command_line, name, value);
+	}
+}
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "%s\n", saved_command_line);
+#ifdef CONFIG_MACH_LGE
+	if (lge_get_boot_mode() == LGE_BOOT_MODE_CHARGERLOGO) {
+		proc_cmdline_set("androidboot.mode", "charger");
+	}
+#endif
+
+	seq_printf(m, "%s\n", updated_command_line);
 	return 0;
 }
 
@@ -23,6 +56,9 @@ static const struct file_operations cmdline_proc_fops = {
 
 static int __init proc_cmdline_init(void)
 {
+	// copy it only once
+	strcpy(updated_command_line, saved_command_line);
+
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
