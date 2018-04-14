39929d06d45b27f2e5f02ea0fcc00670643f61ce
stendro <33956801+stendro@users.noreply.github.com>
2018-04-14 21:27:08 +0200

    build: Add missing condition

--- copy_finished.sh
+++ copy_finished.sh
@@ -63,7 +63,7 @@ COPY_AK() {
 }
 
 COPY_INIT() {
-	if [ "$DEVICE" = "H870" ] || [ "$DEVICE" = "US997" ]; then
+	if [ "$DEVICE" = "H870" ] || [ "$DEVICE" = "US997" ] || [ "$DEVICE" = "H872" ]; then
 	  echo "Copying init file (G6)..."
 	  cp $INIT_FILE_G6 $DDIR/ramdisk/init.blu_active.rc \
 		|| ABORT "Failed to copy init file"
