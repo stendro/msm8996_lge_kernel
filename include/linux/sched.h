164fefe627dcdd3e05b9a7b8bd47a57ae21f93e7
joshuous <joshuous@gmail.com>
2018-05-22 17:36:51 +0200

    Revert "sched: Make the scheduler aware of C-state for cpus"
    This reverts commit 6f0c6eb5633ece055873fa49d598799539a5d628.
    
    Signed-off-by: joshuous <joshuous@gmail.com>
    
     Conflicts:
    	kernel/sched/core.c

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2334,8 +2334,6 @@ extern void do_set_cpus_allowed(struct task_struct *p,
 
 extern int set_cpus_allowed_ptr(struct task_struct *p,
 				const struct cpumask *new_mask);
-extern void sched_set_cpu_cstate(int cpu, int cstate,
-			 int wakeup_energy, int wakeup_latency);
 #else
 static inline void do_set_cpus_allowed(struct task_struct *p,
 				      const struct cpumask *new_mask)
@@ -2347,10 +2345,6 @@ static inline int set_cpus_allowed_ptr(struct task_struct *p,
 	if (!cpumask_test_cpu(0, new_mask))
 		return -EINVAL;
 	return 0;
-}
-static inline void
-sched_set_cpu_cstate(int cpu, int cstate, int wakeup_energy, int wakeup_latency)
-{
 }
 #endif
 
