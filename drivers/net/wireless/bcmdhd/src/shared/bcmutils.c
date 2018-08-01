1d4fb4a71a18949f110b2990fb6bf1c8a1912181
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-01 11:47:54 -0700

    bcmdhd: Fix string literal warning
    Change-Id: Idc267321fe0fcbd7182f573b33e3bc2b2f91ac56

--- drivers/net/wireless/bcmdhd/src/shared/bcmutils.c
+++ drivers/net/wireless/bcmdhd/src/shared/bcmutils.c
@@ -2330,7 +2330,7 @@ bcm_bprintf(struct bcmstrbuf *b, const char *fmt, ...)
 
 	r = vsnprintf(b->buf, b->size, fmt, ap);
 	if (bcm_bprintf_bypass == TRUE) {
-		printf(b->buf);
+		printf("%s\n", b->buf);
 		goto exit;
 	}
 
