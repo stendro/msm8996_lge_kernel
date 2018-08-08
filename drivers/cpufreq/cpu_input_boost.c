6b56ce115b2d86d477b2357d0c8700d17e8b9cf9
Nathan Chancellor <natechancellor@gmail.com>
2018-08-08 14:46:06 -0700

    cpu_input_boost: Remove the actual input boosting functionality
    This probably seems totally idiotic but I don't want to optimize the
    input boosting for this device but I would like the max boost
    functionality for waking up the device and launching apps.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -21,14 +21,11 @@
 
 /* Available bits for boost_drv state */
 #define SCREEN_AWAKE		(1U << 0)
-#define INPUT_BOOST		(1U << 1)
-#define WAKE_BOOST		(1U << 2)
-#define MAX_BOOST		(1U << 3)
+#define WAKE_BOOST		(1U << 1)
+#define MAX_BOOST		(1U << 2)
 
 struct boost_drv {
 	struct workqueue_struct *wq;
-	struct work_struct input_boost;
-	struct delayed_work input_unboost;
 	struct work_struct max_boost;
 	struct delayed_work max_unboost;
 	struct notifier_block cpu_notif;
@@ -40,14 +37,6 @@ struct boost_drv {
 };
 
 static struct boost_drv *boost_drv_g;
-
-static u32 get_boost_freq(struct boost_drv *b, u32 cpu)
-{
-	if (cpumask_test_cpu(cpu, cpu_lp_mask))
-		return CONFIG_INPUT_BOOST_FREQ_LP;
-
-	return CONFIG_INPUT_BOOST_FREQ_PERF;
-}
 
 static u32 get_boost_state(struct boost_drv *b)
 {
@@ -87,22 +76,11 @@ static void update_online_cpu_policy(void)
 
 static void unboost_all_cpus(struct boost_drv *b)
 {
-	if (!cancel_delayed_work_sync(&b->input_unboost) &&
-		!cancel_delayed_work_sync(&b->max_unboost))
+	if (!cancel_delayed_work_sync(&b->max_unboost))
 		return;
 
-	clear_boost_bit(b, INPUT_BOOST | WAKE_BOOST | MAX_BOOST);
+	clear_boost_bit(b, WAKE_BOOST | MAX_BOOST);
 	update_online_cpu_policy();
-}
-
-void cpu_input_boost_kick(void)
-{
-	struct boost_drv *b = boost_drv_g;
-
-	if (!b)
-		return;
-
-	queue_work(b->wq, &b->input_boost);
 }
 
 static void __cpu_input_boost_kick_max(struct boost_drv *b,
@@ -134,28 +112,6 @@ void cpu_input_boost_kick_max(unsigned int duration_ms)
 	__cpu_input_boost_kick_max(b, duration_ms);
 }
 
-static void input_boost_worker(struct work_struct *work)
-{
-	struct boost_drv *b = container_of(work, typeof(*b), input_boost);
-
-	if (!cancel_delayed_work_sync(&b->input_unboost)) {
-		set_boost_bit(b, INPUT_BOOST);
-		update_online_cpu_policy();
-	}
-
-	queue_delayed_work(b->wq, &b->input_unboost,
-		msecs_to_jiffies(CONFIG_INPUT_BOOST_DURATION_MS));
-}
-
-static void input_unboost_worker(struct work_struct *work)
-{
-	struct boost_drv *b =
-		container_of(to_delayed_work(work), typeof(*b), input_unboost);
-
-	clear_boost_bit(b, INPUT_BOOST);
-	update_online_cpu_policy();
-}
-
 static void max_boost_worker(struct work_struct *work)
 {
 	struct boost_drv *b = container_of(work, typeof(*b), max_boost);
@@ -183,7 +139,7 @@ static int cpu_notifier_cb(struct notifier_block *nb,
 {
 	struct boost_drv *b = container_of(nb, typeof(*b), cpu_notif);
 	struct cpufreq_policy *policy = data;
-	u32 boost_freq, state;
+	u32 state;
 
 	if (action != CPUFREQ_ADJUST)
 		return NOTIFY_OK;
@@ -196,16 +152,8 @@ static int cpu_notifier_cb(struct notifier_block *nb,
 		return NOTIFY_OK;
 	}
 
-	/*
-	 * Boost to policy->max if the boost frequency is higher. When
-	 * unboosting, set policy->min to the absolute min freq for the CPU.
-	 */
-	if (state & INPUT_BOOST) {
-		boost_freq = get_boost_freq(b, policy->cpu);
-		policy->min = min(policy->max, boost_freq);
-	} else {
-		policy->min = policy->cpuinfo.min_freq;
-	}
+	/* When unboosting, set policy->min to the absolute min freq for the CPU */
+	policy->min = policy->cpuinfo.min_freq;
 
 	return NOTIFY_OK;
 }
@@ -231,20 +179,6 @@ static int fb_notifier_cb(struct notifier_block *nb,
 	}
 
 	return NOTIFY_OK;
-}
-
-static void cpu_input_boost_input_event(struct input_handle *handle,
-	unsigned int type, unsigned int code, int value)
-{
-	struct boost_drv *b = handle->handler->private;
-	u32 state;
-
-	state = get_boost_state(b);
-
-	if (!(state & SCREEN_AWAKE))
-		return;
-
-	queue_work(b->wq, &b->input_boost);
 }
 
 static int cpu_input_boost_input_connect(struct input_handler *handler,
@@ -312,7 +246,6 @@ static const struct input_device_id cpu_input_boost_ids[] = {
 };
 
 static struct input_handler cpu_input_boost_input_handler = {
-	.event		= cpu_input_boost_input_event,
 	.connect	= cpu_input_boost_input_connect,
 	.disconnect	= cpu_input_boost_input_disconnect,
 	.name		= "cpu_input_boost_handler",
@@ -335,8 +268,6 @@ static int __init cpu_input_boost_init(void)
 	}
 
 	spin_lock_init(&b->lock);
-	INIT_WORK(&b->input_boost, input_boost_worker);
-	INIT_DELAYED_WORK(&b->input_unboost, input_unboost_worker);
 	INIT_WORK(&b->max_boost, max_boost_worker);
 	INIT_DELAYED_WORK(&b->max_unboost, max_unboost_worker);
 	b->state = SCREEN_AWAKE;
