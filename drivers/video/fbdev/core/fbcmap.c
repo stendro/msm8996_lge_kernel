2b5baee7bdbf383af7fc87017c18c29df5c75047
ahmedradaideh <ahmed.radaideh@gmail.com>
2018-10-02 21:57:46 +0300

    fbdev: Remove unused check size
    After this commit 59ab6ac4aa855c38f40ab9e6501b17189e7b4089, Not need it.
    
    This reverts commit 694b0c11423f263361f25acab7139a3cb2840924
    
    Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>

--- drivers/video/fbdev/core/fbcmap.c
+++ drivers/video/fbdev/core/fbcmap.c
@@ -202,8 +202,6 @@ int fb_cmap_to_user(const struct fb_cmap *from, struct fb_cmap_user *to)
 	if (size == 0)
 		return -EINVAL;
 	size *= sizeof(u16);
-	if (size == 0)
-		return -EINVAL;
 
 	if (copy_to_user(to->red+tooff, from->red+fromoff, size))
 		return -EFAULT;
