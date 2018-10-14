59876a1bbc2a192d51c0dde54379978678e8b74a
ahmedradaideh <ahmed.radaideh@gmail.com>
2018-10-14 19:57:12 +0300

    ANDROID: binder: Remove include atomic.h
    This Part from commit a6c412315a0cf272fc0990480f187fa8446fd269
    
    Fixes: ad95c945ce4ba6caac7b41a3225667c5d11290f4 (Merge tag 'google-common-android-3.18' into 'msm-3.18')
    Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -52,7 +52,6 @@
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
 #include <asm/cacheflush.h>
-#include <linux/atomic.h>
 #include <linux/fdtable.h>
 #include <linux/file.h>
 #include <linux/freezer.h>
