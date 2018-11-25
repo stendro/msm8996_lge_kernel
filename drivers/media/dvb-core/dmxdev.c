487ca4dff2599c0673088a236b6a1356298fd069
stendro <33956801+stendro@users.noreply.github.com>
2018-11-25 19:18:38 +0100

    treewide: Fix GCC 7+ compilation errors

--- drivers/media/dvb-core/dmxdev.c
+++ drivers/media/dvb-core/dmxdev.c
@@ -449,7 +449,7 @@ static int dvb_dmxdev_update_events(struct dmxdev_events_queue *events,
 				bytes_read = 0;
 			}
 		} else {
-			if (bytes_read)
+			if (bytes_read) {
 				/*
 				 * data was read beyond the non-data event,
 				 * making it not relevant anymore
@@ -460,6 +460,7 @@ static int dvb_dmxdev_update_events(struct dmxdev_events_queue *events,
 				if (!(events->event_mask.no_wakeup_mask &
 					event->type))
 					events->wakeup_events_counter--;
+			}
 		}
 
 		events->read_index = events->notified_index;
