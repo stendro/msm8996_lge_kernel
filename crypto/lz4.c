cac3b94f3ad43267965a7f6846935cf96986ff22
stendro <33956801+stendro@users.noreply.github.com>
2018-12-04 14:29:49 +0100

    crypto: lz4: Use LZ4_compress_fast
    Use acceleration parameter for faster (de)compress
    at the expense of compression ratio.
    
    Here I picked 4, which is approximately 14% faster compression,
    slightly faster decompression, and a reduced compression ratio (~1.9).
    
    We are now using lz4 v1.8.3 which is already faster on default level
    than v1.7.3 and previous version first introduced in the kernel.

--- crypto/lz4.c
+++ crypto/lz4.c
@@ -48,9 +48,10 @@ static void lz4_exit(struct crypto_tfm *tfm)
 static int lz4_compress_crypto(struct crypto_tfm *tfm, const u8 *src,
 			    unsigned int slen, u8 *dst, unsigned int *dlen)
 {
+	int accel = 4;
 	struct lz4_ctx *ctx = crypto_tfm_ctx(tfm);
-	int out_len = LZ4_compress_default(src, dst,
-		slen, *dlen, ctx->lz4_comp_mem);
+	int out_len = LZ4_compress_fast(src, dst,
+		slen, *dlen, accel, ctx->lz4_comp_mem);
 
 	if (!out_len)
 		return -EINVAL;
