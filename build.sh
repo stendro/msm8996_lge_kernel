fa22cb7ee9b0fca688248bd8cec682fea59a0417
stendro <33956801+stendro@users.noreply.github.com>
2018-12-11 23:36:17 +0100

    build: Use thin archives & dead code elimination with ld.gold

--- build.sh
+++ build.sh
@@ -205,6 +205,9 @@ SETUP_BUILD() {
 	mkdir -p $BDIR
 	make -C "$RDIR" O=$BDIR "$DEVICE_DEFCONFIG" \
 		|| ABORT "Failed to set up build."
+	if [ "$MK_LINKER" = "ld.gold" ]; then
+	  echo "CONFIG_THIN_ARCHIVES=y" >> $BDIR/.config
+	fi
 	if [ "$IS_TWRP" = "twrp" ]; then
 	  echo "include mk2000_twrp_conf" >> $BDIR/.config
 	fi
