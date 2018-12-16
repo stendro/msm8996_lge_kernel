3aaec6024bdf566b73b2b9873ce3c9d8f48cb566
stendro <33956801+stendro@users.noreply.github.com>
2018-12-16 18:23:32 +0100

    drivers: Compile out IPC_LOGGING
    * add msm_smd.c and msm_smem.c to the list

--- drivers/slimbus/slim-msm-ngd.c
+++ drivers/slimbus/slim-msm-ngd.c
@@ -1708,6 +1708,7 @@ static int ngd_slim_probe(struct platform_device *pdev)
 	slim_set_ctrldata(&dev->ctrl, dev);
 
 	/* Create IPC log context */
+#ifdef CONFIG_IPC_LOGGING
 	dev->ipc_slimbus_log = ipc_log_context_create(IPC_SLIMBUS_LOG_PAGES,
 						dev_name(dev->dev), 0);
 	if (!dev->ipc_slimbus_log)
@@ -1719,6 +1720,9 @@ static int ngd_slim_probe(struct platform_device *pdev)
 		SLIM_INFO(dev, "start logging for slim dev %s\n",
 				dev_name(dev->dev));
 	}
+#else
+	dev->ipc_slimbus_log = NULL;
+#endif
 	ret = sysfs_create_file(&dev->dev->kobj, &dev_attr_debug_mask.attr);
 	if (ret) {
 		dev_err(&pdev->dev, "Failed to create dev. attr\n");
