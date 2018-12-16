6ed274c0ddd963920006b3329e36b474fe029352
stendro <33956801+stendro@users.noreply.github.com>
2018-12-16 18:23:32 +0100

    drivers: Compile out IPC_LOGGING

--- drivers/platform/msm/mhi_dev/mhi.c
+++ drivers/platform/msm/mhi_dev/mhi.c
@@ -2917,12 +2917,14 @@ static int mhi_dev_probe(struct platform_device *pdev)
 			pr_err("Error reading MHI Dev DT\n");
 			return rc;
 		}
+#ifdef CONFIG_IPC_LOGGING
 		mhi_ipc_log = ipc_log_context_create(MHI_IPC_LOG_PAGES,
 								"mhi", 0);
 		if (mhi_ipc_log == NULL) {
 			dev_err(&pdev->dev,
 				"Failed to create IPC logging context\n");
 		}
+#endif
 		mhi_uci_init();
 		mhi_update_state_info(MHI_DEV_UEVENT_CTRL,
 						MHI_STATE_CONFIGURED);
