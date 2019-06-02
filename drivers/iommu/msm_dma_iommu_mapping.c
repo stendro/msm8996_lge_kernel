27f591387855b80d31592eab81f972fafd3069ee
Sultan Alsawaf <sultan@kerneltoast.com>
2019-06-02 10:00:52 -0700

    iommu: msm: Fix race due to mapping and unmapping simultaneously
    Stendro: Backport again
    
    A map can occur at the same time as an unmap, which can cause both the
    map and unmap to fail in horrendous ways. Guard against this race by not
    allowing mapping and unmapping for the same iommu meta to occur
    simultaneously.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/iommu/msm_dma_iommu_mapping.c
+++ drivers/iommu/msm_dma_iommu_mapping.c
@@ -39,6 +39,7 @@ struct msm_iommu_meta {
 	struct rb_node node;
 	struct list_head maps;
 	struct kref ref;
+	struct mutex map_lock;
 	rwlock_t lock;
 	void *buffer;
 };
@@ -138,7 +139,10 @@ static void msm_iommu_map_destroy(struct kref *kref)
 	list_del(&map->lnode);
 	write_unlock(&meta->lock);
 
+	mutex_lock(&meta->map_lock);
 	dma_unmap_sg(map->dev, &map->sgl, map->nents, map->dir);
+	mutex_unlock(&meta->map_lock);
+
 	kfree(map);
 }
 
@@ -157,6 +161,7 @@ static struct msm_iommu_meta *msm_iommu_meta_create(struct dma_buf *dma_buf)
 
 	meta->buffer = dma_buf->priv;
 	kref_init(&meta->ref);
+	mutex_init(&meta->map_lock);
 	rwlock_init(&meta->lock);
 	INIT_LIST_HEAD(&meta->maps);
 	msm_iommu_meta_add(meta);
@@ -202,7 +207,9 @@ static int __msm_dma_map_sg(struct device *dev, struct scatterlist *sg,
 			goto release_meta;
 		}
 
+		mutex_lock(&meta->map_lock);
 		ret = dma_map_sg_attrs(dev, sg, nents, dir, attrs);
+		mutex_unlock(&meta->map_lock);
 		if (ret != nents) {
 			kfree(map);
 			goto release_meta;
@@ -288,6 +295,28 @@ void msm_dma_unmap_sg(struct device *dev, struct scatterlist *sgl, int nents,
 	kref_put(&meta->ref, msm_iommu_meta_destroy);
 }
 
+static void msm_dma_unmap_list(struct list_head *unmap_list)
+{
+	struct msm_iommu_map *map, *map_next;
+	struct msm_iommu_meta *meta;
+	LIST_HEAD(kfree_list);
+
+	while (!list_empty(unmap_list)) {
+		meta = list_first_entry(unmap_list, typeof(*map), lnode)->meta;
+		mutex_lock(&meta->map_lock);
+		list_for_each_entry_safe(map, map_next, unmap_list, lnode) {
+			if (map->meta != meta)
+				break;
+			dma_unmap_sg(map->dev, &map->sgl, map->nents, map->dir);
+			list_move_tail(&map->lnode, &kfree_list);
+		}
+		mutex_unlock(&meta->map_lock);
+	}
+
+	list_for_each_entry_safe(map, map_next, &kfree_list, lnode)
+		kfree(map);
+}
+
 /* Only to be called by ION code when a buffer is freed */
 void msm_dma_buf_freed(void *buffer)
 {
@@ -307,10 +336,7 @@ void msm_dma_buf_freed(void *buffer)
 	}
 	write_unlock(&meta->lock);
 
-	list_for_each_entry_safe(map, map_next, &unmap_list, lnode) {
-		dma_unmap_sg(map->dev, &map->sgl, map->nents, map->dir);
-		kfree(map);
-	}
+	msm_dma_unmap_list(&unmap_list);
 
 	/* Do an extra put to undo msm_iommu_meta_lookup_get */
 	kref_put(&meta->ref, msm_iommu_meta_destroy);
