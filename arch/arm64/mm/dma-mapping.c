258716e12fcdc9a6367bc6b6441feb9c064c303d
stendro <33956801+stendro@users.noreply.github.com>
2019-06-10 14:45:43 +0200

    Import LGE bcmdhd wifi drivers (latest oreo source)
    From H850-30F, or any of the latest G5/G6/V20 Oreo sources,
    as they're mostly the same. Nicely unified this time around.

--- arch/arm64/mm/dma-mapping.c
+++ arch/arm64/mm/dma-mapping.c
@@ -68,7 +68,7 @@ static bool is_dma_coherent(struct device *dev, struct dma_attrs *attrs)
 
 static struct gen_pool *atomic_pool;
 #define NO_KERNEL_MAPPING_DUMMY 0x2222
-#define DEFAULT_DMA_COHERENT_POOL_SIZE  SZ_256K
+#define DEFAULT_DMA_COHERENT_POOL_SIZE  SZ_2M /* BCMDHD for Wi-Fi */
 static size_t atomic_pool_size __initdata = DEFAULT_DMA_COHERENT_POOL_SIZE;
 
 static int __init early_coherent_pool(char *p)
--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -7628,7 +7628,11 @@ static int nl80211_dump_survey(struct sk_buff *skb, struct netlink_callback *cb)
 static bool nl80211_valid_wpa_versions(u32 wpa_versions)
 {
 	return !(wpa_versions & ~(NL80211_WPA_VERSION_1 |
-				  NL80211_WPA_VERSION_2));
+				  NL80211_WPA_VERSION_2
+#ifdef CONFIG_BRCM_WAPI
+                               | NL80211_WAPI_VERSION_1
+#endif
+                 ));
 }
 
 static int nl80211_authenticate(struct sk_buff *skb, struct genl_info *info)
