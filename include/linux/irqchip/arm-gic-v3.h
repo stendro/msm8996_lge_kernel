680ffa6ab25f83c078d3523ef0c9094e5065d20a
Sultan Alsawaf <sultan@kerneltoast.com>
2020-03-18 21:54:15 -0700

    irqchip/gic-v3-its: Put its_init() in __init section
    This fixes a section mismatch since its_init() is only used from inside
    the __init section.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- include/linux/irqchip/arm-gic-v3.h
+++ include/linux/irqchip/arm-gic-v3.h
@@ -358,8 +358,8 @@ static inline void gic_write_eoir(u64 irq)
 
 struct irq_domain;
 int its_cpu_init(void);
-int its_init(struct device_node *node, struct rdists *rdists,
-	     struct irq_domain *domain);
+int __init its_init(struct device_node *node, struct rdists *rdists,
+		    struct irq_domain *parent_domain);
 
 #endif
 
