f9c58f097a7c4c7694aedb9cd60102bd583648db
Ahmad Thoriq Najahi <najahi@chips-projects.xyz>
2020-06-20 20:27:33 +0700

    msm: camera_v2: Fix qos update warning
    qos requirement is not inited when update is called.
    
    [...]
    [   27.206894] msm_pm_qos_update_request: update request 100
    [   27.206900] msm_pm_qos_add_request: add request
    [   27.206904] pm_qos_update_request() called for unknown object
    [   27.206915] ------------[ cut here ]------------
    [   27.206928] WARNING: CPU: 5 PID: 1107 at kernel/power/qos.c:696 pm_qos_update_request+0x4c/0x60
    [   27.206935] CPU: 5 PID: 1107 Comm: qvrservice Tainted: G S              4.14.185-Chips #1
    [   27.206937] Hardware name: Qualcomm Technologies, Inc. trinket pm6125 + pmi632 IDP (DT)
    [   27.206939] task: 00000000ffaf085c task.stack: 000000007a0f23be
    [   27.206942] pc : pm_qos_update_request+0x4c/0x60
    [   27.206943] lr : pm_qos_update_request+0x4c/0x60
    [...]
    
    Signed-off-by: Ahmad Thoriq Najahi <najahi@chips-projects.xyz>

--- drivers/media/platform/msm/camera_v2/msm.c
+++ drivers/media/platform/msm/camera_v2/msm.c
@@ -251,7 +251,8 @@ static void msm_pm_qos_remove_request(void)
 void msm_pm_qos_update_request(int val)
 {
 	pr_info("%s: update request %d", __func__, val);
-	pm_qos_update_request(&msm_v4l2_pm_qos_request, val);
+	if (pm_qos_request_active(&msm_v4l2_pm_qos_request))
+		pm_qos_update_request(&msm_v4l2_pm_qos_request, val);
 }
 
 struct msm_session *msm_session_find(unsigned int session_id)
