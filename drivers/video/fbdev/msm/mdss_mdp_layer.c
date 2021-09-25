854040819a2ade104e8af2735f2bdaf4ef6e66e5
stendro <33956801+stendro@users.noreply.github.com>
2021-09-25 13:23:26 +0200

    treewide: warning: this 'if' clause does not guard...
    GCC 12 detected these

--- drivers/video/fbdev/msm/mdss_mdp_layer.c
+++ drivers/video/fbdev/msm/mdss_mdp_layer.c
@@ -1317,10 +1317,10 @@ static int __configure_pipe_params(struct msm_fb_data_type *mfd,
 			BLEND_OP_PREMULTIPLIED : BLEND_OP_OPAQUE;
 
 	if (pipe->src_fmt->is_yuv && !(pipe->flags & MDP_SOURCE_ROTATED_90) &&
-			!pipe->scaler.enable) {
+			!pipe->scaler.enable)
 		pipe->overfetch_disable = OVERFETCH_DISABLE_BOTTOM;
 
-	if (pipe->dst.x >= left_lm_w)
+	if (pipe->dst.x >= left_lm_w) {
 		pipe->overfetch_disable |= OVERFETCH_DISABLE_RIGHT;
 		pr_debug("overfetch flags=%x\n", pipe->overfetch_disable);
 	} else {
