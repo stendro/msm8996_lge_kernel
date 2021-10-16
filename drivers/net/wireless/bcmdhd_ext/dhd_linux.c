884fcc17c32a2945f9ed744f6ab82f8392979465
Shobek Sam Attupurath <sattupurath@nvidia.com>
2021-10-16 04:43:39 +0200

    wireless: bcmdhd: Update timing of TDLS set mode
    Issue: tdls_set_mode fails if it is called before firmware
    capabilities are loaded
    
    Fix: Invoke tdls_set_mode after firmware capabilities are loaded
    
    Bug 200387597
    
    Change-Id: I1a96d3f86591fd513507075e463ad031aef2e8ee
    Signed-off-by: Shobek Sam Attupurath <sattupurath@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1680638
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Srinivas Ramachandran <srinivasra@nvidia.com>
    Reviewed-by: Nagaraj Annaiah <nannaiah@nvidia.com>
    Reviewed-by: Ashutosh Jha <ajha@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/net/wireless/bcmdhd_ext/dhd_linux.c
+++ drivers/net/wireless/bcmdhd_ext/dhd_linux.c
@@ -8924,10 +8924,6 @@ dhd_preinit_ioctls(dhd_pub_t *dhd)
 	dhd->apf_set = FALSE;
 #endif /* APF */
 #endif /* PKT_FILTER_SUPPORT */
-#ifdef WLTDLS
-	dhd->tdls_enable = FALSE;
-	dhd_tdls_set_mode(dhd, false);
-#endif /* WLTDLS */
 	dhd->suspend_bcn_li_dtim = CUSTOM_SUSPEND_BCN_LI_DTIM;
 #ifdef ENABLE_MAX_DTIM_IN_SUSPEND
 	dhd->max_dtim_enable = TRUE;
@@ -9019,6 +9015,10 @@ dhd_preinit_ioctls(dhd_pub_t *dhd)
 #if defined(ARP_OFFLOAD_SUPPORT)
 			arpoe = 0;
 #endif
+#ifdef WLTDLS
+	dhd->tdls_enable = FALSE;
+	dhd_tdls_set_mode(dhd, false);
+#endif /* WLTDLS */
 #ifdef PKT_FILTER_SUPPORT
 			dhd_pkt_filter_enable = FALSE;
 #endif
