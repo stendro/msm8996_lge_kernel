31e28ed59220df7c13d0ddde2082d7e8eab794c8
stendro <33956801+stendro@users.noreply.github.com>
2021-11-01 12:21:44 +0100

    Revert "sock: inline SELinux's sk_security to struct sock"
    This reverts commit 759ce475b1926c29c08934df37fe1be97c2313b8.

--- include/net/sock.h
+++ include/net/sock.h
@@ -218,22 +218,6 @@ struct sock_common {
 	/* public: */
 };
 
-struct sk_security_struct {
-#ifdef CONFIG_NETLABEL
-	enum {				/* NetLabel state */
-		NLBL_UNSET = 0,
-		NLBL_REQUIRE,
-		NLBL_LABELED,
-		NLBL_REQSKB,
-		NLBL_CONNLABELED,
-	} nlbl_state;
-	struct netlbl_lsm_secattr *nlbl_secattr; /* NetLabel sec attributes */
-#endif
-	u32 sid;			/* SID of this object */
-	u32 peer_sid;			/* SID of peer */
-	u16 sclass;			/* sock security class */
-};
-
 struct cg_proto;
 /**
   *	struct sock - network layer representation of sockets
@@ -440,7 +424,7 @@ struct sock {
 	__s32			sk_peek_off;
 	int			sk_write_pending;
 #ifdef CONFIG_SECURITY
-	struct sk_security_struct	sk_security[1];
+	void			*sk_security;
 #endif
 	__u32			sk_mark;
 	kuid_t			sk_uid;
