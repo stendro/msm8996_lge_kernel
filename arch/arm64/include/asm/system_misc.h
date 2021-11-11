1d5a0b0530815ccb01e2f7ae71c6a6316db27ba1
stendro <33956801+stendro@users.noreply.github.com>
2021-11-11 18:08:21 +0100

    arm64: Add system_rev support

--- arch/arm64/include/asm/system_misc.h
+++ arch/arm64/include/asm/system_misc.h
@@ -46,6 +46,8 @@ extern void __show_regs(struct pt_regs *);
 extern void (*arm_pm_restart)(enum reboot_mode reboot_mode, const char *cmd);
 extern char* (*arch_read_hardware_id)(void);
 
+extern unsigned int system_rev;
+
 #define show_unhandled_signals_ratelimited()				\
 ({									\
 	static DEFINE_RATELIMIT_STATE(_rs,				\
