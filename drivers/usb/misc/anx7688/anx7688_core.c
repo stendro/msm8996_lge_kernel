cefeea841c2b4a7bc2118466ef509136cb2eb7bd
AShiningRay <genesis45.ppc@gmail.com>
2021-11-12 14:19:28 -0300

    Small changes that help compile for the V20.
    Some of those are bypasses, so an issue will be created to track
    them.

--- drivers/usb/misc/anx7688/anx7688_core.c
+++ drivers/usb/misc/anx7688/anx7688_core.c
@@ -2613,8 +2613,8 @@ static int anx7688_resume(struct device *dev)
 }
 
 static const struct dev_pm_ops anx7688_dev_pm_ops = {
-	.suspend = anx7688_suspend,
-	.resume  = anx7688_resume,
+	//.suspend = anx7688_suspend,
+	//.resume  = anx7688_resume,
 };
 #endif
 
