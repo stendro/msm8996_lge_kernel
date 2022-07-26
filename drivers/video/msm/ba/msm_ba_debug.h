b7266a6d3ea724e82d64273e8ffac9af461b0f80
AShiningRay <genesis45.ppc@gmail.com>
2022-07-26 12:32:34 -0300

    kernel: tracing: Disable instances of trace_printk() on lge 8996
    Those calls make the kernel allocate extra memory for addditional
    buffers. Leaving trace_printk enabled on code already slated for
    release isn't a good practice as well, so we're better off
    disabling it as we're not going to trace bugs on those files.

--- drivers/video/msm/ba/msm_ba_debug.h
+++ drivers/video/msm/ba/msm_ba_debug.h
@@ -66,10 +66,10 @@ extern int msm_ba_debug_out;
 						BA_MSG_PRIO2STRING(__level), \
 						## arg); \
 			} else if (msm_ba_debug_out == BA_OUT_FTRACE) { \
-				trace_printk(KERN_DEBUG BA_DBG_TAG __fmt "\n", \
+				/* trace_printk(KERN_DEBUG BA_DBG_TAG __fmt "\n", \
 						__LINE__, \
 						BA_MSG_PRIO2STRING(__level), \
-						## arg); \
+						## arg); */ \
 			} \
 		} \
 	} while (0)
