a859f34d4cb285be55dc4f322d95d43a5e33824f
stendro <33956801+stendro@users.noreply.github.com>
2022-09-29 23:21:54 +0200

    build: Completely update to latest AK3
    It's been like 3-4 years since I updated the backend. With A12 coming,
    it might be a good idea to have the latest AK, to be sure there's no
    issue due to old code.
    
    Head:
    https://github.com/osm0sis/AnyKernel3/commit/70fb859ec10002abbd459d25c0ca4a9236676aeb

--- mk2000/ak-template.sh
+++ mk2000/ak-template.sh
@@ -32,7 +32,7 @@ properties() { '
 kernel.string=$DEVICE Kernel by askermk2000 @ xda-developers
 do.devicecheck=1
 do.modules=0
-do.ssdtrim=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=$AK_DEV
@@ -41,12 +41,15 @@ device.name3=$DEV_LOW
 device.name4=$NAME_LOW
 device.name5=$DEV_ABOOT
 device.name6=$ABOOT_LOW
+supported.versions=
+supported.patchlevels=
 '; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
@@ -54,8 +57,8 @@ ramdisk_compression=auto;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
-chmod -R 750 \$ramdisk/*;
-chown -R root:root \$ramdisk/*;
+set_perm_recursive 0 0 755 644 \$ramdisk/*;
+set_perm_recursive 0 0 750 750 \$ramdisk/init* \$ramdisk/sbin;
 
 ## AnyKernel install
 dump_boot;
