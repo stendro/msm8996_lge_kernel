93b4b9a189dedb49615ece75dc122a67f051b68e
stendro <33956801+stendro@users.noreply.github.com>
2023-03-07 00:19:06 +0100

    Revert "FROMLIST: [PATCH v2 1/3] arm64: compat: Split the sigreturn trampolines and kuser helpers (C sources)"
    This reverts commit c23a2aa177195431add3fc3398a9db46b416cc87.

--- arch/arm64/include/asm/signal32.h
+++ arch/arm64/include/asm/signal32.h
@@ -20,6 +20,8 @@
 #ifdef CONFIG_COMPAT
 #include <linux/compat.h>
 
+#define AARCH32_KERN_SIGRET_CODE_OFFSET	0x500
+
 int compat_setup_frame(int usig, struct ksignal *ksig, sigset_t *set,
 		       struct pt_regs *regs);
 int compat_setup_rt_frame(int usig, struct ksignal *ksig, sigset_t *set,
